9614745405e484a24f1ac11f1eb05956cabd4f39
pedramjlo <pedram_sif@tuta.io>
2025-01-16 00:47:26 +0330

    set up static route

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -3,6 +3,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import HomeView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", HomeView.as_view(), name='home'),
+
 ]
--- user_account/forms.py
+++ user_account/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
-
+# form for handling user login
 class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ["username", "password"]
+
+
+# form for handling user registry
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
