b3f05fcc1f08383c4d3379a51337b5e6d574de12
pedramjlo <pedram_sif@tuta.io>
2025-01-16 11:53:26 +0330

    added signup functionality

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
-from .views import LoginView
+from .views import LoginView, SignupView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name='login'),
-
+    path("signup/", SignupView.as_view(), name='signup'),
 ]
--- user_account/views.py
+++ user_account/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.views.generic import FormView
 
-from .forms import LoginForm
+from django.contrib.auth.models import User
+
+from .forms import LoginForm, SignupForm
 
 
 
@@ -22,3 +24,19 @@ class LoginView(FormView):
             return redirect(self.success_url)
         else:
             return render(self.request, self.template_name, {'form': form, 'error': 'Invalid credentials'})
+
+
+
+class SignupView(FormView):
+    form_class = SignupForm
+    template_name = 'auth/signup.html'
+    success_url = "/home"
+
+
+    def form_valid(self, form):
+        user=form.save()
+        user.set_password(form.cleaned_data["password1"]) 
+        user.save()
+        login(self.request, user)
+        return redirect(self.success_url)
+    
