e78b3f511b90e04caf4baa71d120e4e6ba5ece83
pedramjlo <pedram_sif@tuta.io>
2025-01-17 17:06:46 +0330

    removed card options

--- static/js/main.js
+++ static/js/main.js
@@ -3,23 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     categories.forEach(function(category) {
         var color = category.getAttribute('data-color') || 'white';  // Default to white if color is not provided
-
-        console.log(category, color);  // Logging element and color
-
-        category.style.backgroundColor = color;  
-        var colorSquares = category.querySelectorAll('.category-color-square');
-        
-        colorSquares.forEach(function(square) {
-            square.style.backgroundColor = color;
-        });
+        console.log(category, color); // Logging element and color
+        category.style.backgroundColor = color;
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
     const flashcards = document.querySelectorAll('.flashcard');
-    const selectElements = document.querySelectorAll('.card-options select');
-
     flashcards.forEach(flashcard => {
         flashcard.addEventListener('click', () => {
             flashcard.classList.toggle('flipped'); // Toggle flip on card click
@@ -43,13 +31,29 @@ document.addEventListener('DOMContentLoaded', function() {
                 svgElement.innerHTML = `<path fill="red" d="M20.212 6.234a1.556 1.556 0 0 1-.363 1.713l-1.282 1.282-.02 6.76 1.325 1.325a1.558 1.558 0 0 1-1.1 2.658l-3.894-.001.001 7.269a.736.736 0 1 1-1.474 0l-.002-7.27-3.868-.026a1.557 1.557 0 0 1-1.086-2.633l1.319-1.318-.038-6.745-1.314-1.314a1.55 1.55 0 0 1 1.095-2.653l9.253.002a1.55 1.55 0 0 1 1.448.95Z"/>`;
             }
 
-            // Add  AJAX request here 
-            selectElements.forEach(select => { select.addEventListener('change', function() { 
-                const flashcardId = select.closest('.flashcard-container').dataset.flashcardId; 
-                const isLearned = select.id === 'is_learned' ? select.value === 'Yes' : undefined; 
-                const isPinned = select.id === 'is_pinned' ? select.value === 'Yes' : undefined; 
-                fetch('/update-flashcard/', { method: 'POST', headers: { 
-                    'Content-Type': 'application/json', 'X-CSRFToken': csrfToken } // Ensure CSRF token is included }, body: JSON.stringify({ flashcardId, isLearned, isPinned }) }) .then(response => response.json()) .then(data => { // Handle the response, update UI if needed }); }); });
+            // Handle your AJAX request here to change the `is_pinned` state
+        });
+    });
+
+    const selectElements = document.querySelectorAll('.card-options select');
+    selectElements.forEach(select => {
+        select.addEventListener('change', function() {
+            const flashcardId = select.closest('.flashcard-container').dataset.flashcardId;
+            const isLearned = select.id === 'is_learned' ? select.value === 'Yes' : undefined;
+            const isPinned = select.id === 'is_pinned' ? select.value === 'Yes' : undefined;
+
+            // AJAX request to update is_learned and is_pinned states
+            fetch('/update-flashcard/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken // Ensure CSRF token is included
+                },
+                body: JSON.stringify({ flashcardId, isLearned, isPinned })
+            }).then(response => response.json())
+              .then(data => {
+                  // Handle the response, update UI if needed
+              });
         });
     });
 });
